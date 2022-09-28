93e556d00a8efa0f56a5f8be4703894150c389f1
Sandeshpokhrel <yespokhrel9999@gmail.com>
2022-09-28 22:05:59 +0545

    hello/

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 const colors = require('colors');
 const http = require('http');
-const Content = require('./routes');
+const Content = require('./routes'); // link another js file in this one i.e. routes.js
 const server = http.createServer(Content);
 server.listen(2022);
 console.log('hello'.green);
